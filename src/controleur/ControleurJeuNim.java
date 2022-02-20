b701d01d32696f19411dfa1d3654ebfcde414431
Nicolas <schivre.nicolas@gmail.com>
2022-02-20 13:58:14 +0100

    Ihm 2/4/22

--- src/controleur/ControleurJeuNim.java
+++ src/controleur/ControleurJeuNim.java
@@ -1,44 +1,121 @@
 package controleur;
 import modele.*;
 import vue.*;
-import java.util.ArrayList;
 
 public class ControleurJeuNim {
-    private Ihm ihm;
-    private CoupNim leCoup;
-    private Tas letas;
-    private ArrayList<Joueur> lesJoueurs = new ArrayList<>();
+    private Ihm leIhm;
+    private Tas lesTas;
+    private Joueur j1;
+    private Joueur j2;
+    private Joueur jCourant;
 
     public ControleurJeuNim(Ihm ihm){
-        this.ihm=ihm;
+        this.leIhm =ihm;
     }
 
-    public void setLeCoup(CoupNim leCoup) {
-        this.leCoup = leCoup;
+    private int creerPartie(){
+        leIhm.setControleurJeuNim(this);
+        int nbTas = 0;
+        while (nbTas<=0){
+            nbTas = leIhm.nombreTas();
+        }
+        lesTas = new Tas(nbTas);
+        lesTas.initialiser();
+        j1 = new Joueur(leIhm.nomJoueur(1));
+        j2 = new Joueur(leIhm.nomJoueur(2));
+        return nbTas;
     }
 
-    public CoupNim creerCoupNim(int numTas, int nbAll){
-        return new CoupNim(numTas,nbAll);
+    private void recreerPartie(int nbTas){
+        lesTas = new Tas(nbTas);
+        lesTas.initialiser();
     }
 
-    public void etatPartie(Tas lestas){
-        ihm.etatPartie(lestas.toString());
+    private void etatPartie(Tas lesTas){
+        leIhm.etatPartie(lesTas.toString());
     }
 
-    public void rejouer(Joueur j){
+    public CoupNim creerCoupNim(int numTas, int nbAll) throws CoupInvalideException {
+       return new CoupNim(numTas,nbAll);
+    }
+
+    private void demandeLeCoup(Joueur j) throws CoupInvalideException {
         String nom = j.getNom();
-        ihm.rejouer(nom);
+        leIhm.leCoup(nom);
     }
 
-    public void leCoup(Joueur j) {
-        String nom = j.getNom();
-        ihm.leCoup(this, nom);
+    public void faireLeCoup(CoupNim coup) throws CoupInvalideException{
+        lesTas.gererCoup(coup);
     }
 
-    public void afficherVainqueurTotal(Joueur j){
+    public void faireUnTour(Joueur j){
+        etatPartie(lesTas);
+        boolean flag1 = true;
+        while (flag1){
+            try{
+                demandeLeCoup(j);
+                flag1 = false;
+            }
+            catch (CoupInvalideException e){
+                e.getMessage();
+            }
+        }
+    }
+
+    public int rejouer(Joueur j){
         String nom = j.getNom();
-        int nbpartiesg = j.getNbPartiesGagnees();
-        ihm.afficherVainqueurTotal(nom,nbpartiesg);
+        return leIhm.rejouer(nom);
+    }
+
+    private void afficherVainqueurTotal(Joueur j1, Joueur j2){
+        if(j1.getNbPartiesGagnees()> j2.getNbPartiesGagnees()){
+            String nom = j1.getNom();
+            int nbpartiesg = j1.getNbPartiesGagnees();
+            leIhm.afficherVainqueurTotal(nom,nbpartiesg);
+        }
+        else if (j1.getNbPartiesGagnees()< j2.getNbPartiesGagnees()){
+            String nom = j2.getNom();
+            int nbpartiesq = j2.getNbPartiesGagnees();
+            leIhm.afficherVainqueurTotal(nom,nbpartiesq);
+        }
+        else{
+            leIhm.egalite(j1.getNbPartiesGagnees());
+        }
+    }
+
+
+    public void jouer(){
+        boolean rejouer = true;
+        int nbTas = creerPartie();
+        while(rejouer){
+            rejouer = false;
+            jCourant = j1;
+            while (true){
+                faireUnTour(jCourant);
+                if(lesTas.partieTerminee()){
+                    Joueur gagnant = jCourant;
+                    gagnant.gagnePartie();
+                    int recommencer = rejouer(gagnant);
+                    if(recommencer==1){
+                        rejouer = true;
+                        recreerPartie(nbTas);
+                        break;
+                    }
+                    else{
+                        leIhm.afficherScore(j1.getNom(),j1.getNbPartiesGagnees(),j2.getNom(), j2.getNbPartiesGagnees());
+                        afficherVainqueurTotal(j1,j2);
+                        break;
+                    }
+                }
+                else if(jCourant==j1){
+                    jCourant = j2;
+                }
+                else{
+                    jCourant = j1;
+                }
+            }
+
+        }
     }
 
 
--- src/vue/Ihm.java
+++ src/vue/Ihm.java
@@ -1,67 +1,78 @@
 package vue;
+
 import java.util.Scanner;
+
 import controleur.*;
+import modele.CoupInvalideException;
 
 public class Ihm {
     private final Scanner sc = new Scanner(System.in);
+    private ControleurJeuNim ctl;
+
+    public void setControleurJeuNim(ControleurJeuNim controleur) {
+        this.ctl = controleur;
+    }
 
     public int nombreTas() {
-        while(true){
-            System.out.println("Veuillez entrer le nombre de tas >0 avec lequel vous voulez jouer.");
-            if(sc.hasNextInt()) {
+        System.out.println("Veuillez entrer le nombre de tas >0 avec lequel vous voulez jouer.");
+        while (true) {
+            if (sc.hasNextInt()) {
                 return sc.nextInt();
-            }
-            else{
+            } else {
                 System.out.println("Il faut entrer un nombre.");
+                sc.next();
             }
         }
     }
 
-    public String nomJoueur(int num){
-        System.out.println("Veuillez entrer le nom du joueur "+num+".");
+    public String nomJoueur(int num) {
+        System.out.println("Veuillez entrer le nom du joueur " + num + ".");
         return sc.next();
     }
 
-    public void etatPartie(String letas){
+    public void etatPartie(String letas) {
         System.out.println(letas);
     }
 
-    public void leCoup(ControleurJeuNim ctl, String nom){
-        while(true){
-            System.out.println(nom + " à vous de jouer un coup sous la forme 'm n' où m est le tas choisi et n le nombre d'allumettes à retirer dans ce tas.");
-            if(sc.hasNextInt()) {
-                int numTas = sc.nextInt();
-                if(sc.hasNextInt()){
-                    int nbAl = sc.nextInt();
-                    ctl.setLeCoup(ctl.creerCoupNim(numTas,nbAl));
-                }
+    public void leCoup(String nom) throws CoupInvalideException {
+        System.out.println(nom + " à vous de jouer un coup sous la forme 'm n' où m est le tas choisi et n le nombre d'allumettes à retirer dans ce tas.");
+        if (sc.hasNextInt()) {
+            int numTas = sc.nextInt();
+            if (sc.hasNextInt()) {
+                int nbAl = sc.nextInt();
+                ctl.faireLeCoup(ctl.creerCoupNim(numTas, nbAl));
             }
-            else{
-                System.out.println("Le coup est invalide rejouez.");
-            }
+        } else {
+            sc.next();
         }
     }
 
-    public int rejouer(String nom){
-        while(true){
-            System.out.println(nom+" as gagné cette partie. Voulez vous rejouer ? 1 pour rejouer ou 0 sinon.");
-            if(sc.hasNextInt()){
+    public int rejouer(String nom) {
+        System.out.println(nom + " as gagné cette partie. Voulez vous rejouer ? 1 pour rejouer ou 0 sinon.");
+        while (true) {
+            if (sc.hasNextInt()) {
                 int choix = sc.nextInt();
-                if(choix==1 || choix ==0){
+                if (choix == 1 || choix == 0) {
                     return choix;
                 }
-            }
-            else{
+            } else {
                 System.out.println("Il faut entrer 0 ou 1 !");
+                sc.next();
             }
         }
     }
 
-    public void afficherVainqueurTotal(String nom, int nbpartiesg){
-        System.out.println("Le gagnant est "+nom+" avec un total de " + nbpartiesg + " parties gagnées !");
+    public void afficherVainqueurTotal(String nom, int nbpartiesG) {
+        System.out.println("Le gagnant est " + nom + " avec un total de " + nbpartiesG + " parties gagnées !");
+    }
+    public void afficherScore(String nomj1, int gagnej1, String nomj2, int gagnej2){
+        System.out.println("Le score de "+nomj1+" est de "+gagnej1+" parties gagnées \n" +
+                           "Le score de "+nomj2+" est de "+gagnej2+" parties gagnées");
     }
 
-
+    public void egalite(int nbpartiesq){
+        System.out.println("Il y a une parfaite égalité avec un nombre de victoire de "+nbpartiesq+" chacun !");
+    }
 
 
 }
